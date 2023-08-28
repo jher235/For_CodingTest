63467470d19ba9b46df1ebd46ef7acec21cc43fa
jher235 <tim668666@gmail.com>
2023-08-28 19:57:23 +0900

    첫 생성

---
+++ python/BJ_17626.py
@@ -0,0 +1,29 @@
+# 계획 1. 제곱수로 이루어진 수 들을 저장하며 해결
+# 계획 2. 제곱수 자체를 저장하여 사용하며 해결
+# 계획 3. 제
+
+
+dplist = []
+
+dplist[1] = 1
+
+num = int(input())
+
+for i in range(num):
+
+    for j in range(2,)
+
+
+
+
+
+
+
+
+
+
+
+
+n = int(input())
+
+
