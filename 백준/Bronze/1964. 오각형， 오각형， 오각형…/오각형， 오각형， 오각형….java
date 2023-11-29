cbb6d90bd200603ea8e1be2902dd849d163867b4
jher235 <127422627+jher235@users.noreply.github.com>
2023-11-29 22:53:46 +0900

    [Bronze II] Title: 오각형, 오각형, 오각형…, Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/1964. 오각형， 오각형， 오각형…/오각형， 오각형， 오각형….java
@@ -0,0 +1,37 @@
+//import java.util.Scanner;
+//import java.util.ArrayList;
+//import java.util.LinkedList;
+//import java.util.Queue;
+//import java.util.Arrays;
+//import java.util.Comparator;
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.*;
+import java.sql.Array;
+
+//import java.io.BufferedReader;
+//import java.io.BufferedWriter;
+//import java.io.InputStreamReader;
+//import java.io.OutputStreamWriter;
+//import java.math.BigInteger;
+//import java.io.IOException;
+
+//import java.nio.Buffer;
+//import java.sql.Array;
+
+public class Main{
+	
+	
+	
+	public static void main(String args[]) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextInt();
+		long ans =0;
+		ans = (((3*n*n)+(5*n)+2)/2)%45678;
+		System.out.print(ans);
+		
+		
+	}
+}
